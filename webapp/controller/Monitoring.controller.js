cecd125964b0599f7b8bd5d4fd43759d52e5b5b5
String Shi <junteng007@live.cn>
2020-02-20 11:01:23 +0800

    2020-02-20 Add Hospital

--- webapp/controller/Monitoring.controller.js
+++ webapp/controller/Monitoring.controller.js
@@ -19,6 +19,14 @@ sap.ui.define([
 				this.getView().addDependent(this._oBlockDetailPopover);
 			}.bind(this));
 
+			Fragment.load({
+				name: "sap.ibso.skyeyeForCdc.view.fragment.HospitalDetail",
+				controller: this
+			}).then(function (oPopover) {
+				this._oHospitalDetailPopover = oPopover;
+				this.getView().addDependent(this._oHospitalDetailPopover);
+			}.bind(this));
+
 			this.getView().setModel(new sap.ui.model.json.JSONModel("json/orgUnit.json"),"orgUnit");
 			this.getView().setModel(new sap.ui.model.json.JSONModel("json/block.json"), "block");
 			this.getView().setModel(new sap.ui.model.json.JSONModel("json/hospital.json"), "hospital");
@@ -30,18 +38,18 @@ sap.ui.define([
 			this.getView().setModel(new sap.ui.model.json.JSONModel("json/graphTest.json"));
 
 			this.getView().setModel(new sap.ui.model.json.JSONModel({
-				AreaRiskVisible:true,
+				BlockRiskVisible:true,
 				CaseVisible:true,
 				CaseTraceVisible:true,
 				HospitalVisible:true,
-				AreaRiskLevel:"4",
+				BlockRiskLevel:"4",
 				AutoLevel:false
 			}),"pageConfig");
 
 			// this.getView().byId("caseDetailContainer").bindElement("case>/0");
 
 			// this.getView().byId("fdl").setStaticNodes([0,1,2,3,4,5,6]);
-			this.onLevelChange();
+			this.onChangeLevel();
 		},
 
 		onGraphReady:function(oEvent){
@@ -52,14 +60,19 @@ sap.ui.define([
 			
 		},
 
-		onPressProject: function (oEvent) {
+		onPressBlock: function (oEvent) {
 			this._oBlockDetailPopover.bindElement("block>" + oEvent.getSource().getBindingContext("block").getPath());
 			this._oBlockDetailPopover.openBy(oEvent.getSource());
 		},
 
+		onPressHospital: function (oEvent) {
+			this._oHospitalDetailPopover.bindElement("hospital>" + oEvent.getSource().getBindingContext("hospital").getPath());
+			this._oHospitalDetailPopover.openBy(oEvent.getSource());
+		},
+
 		onPressCase: function (oEvent) {
 			var oDetailContainer = this.getView().byId("caseDetailContainer");
-			oDetailContainer.bindElement("case>" + oEvent.getSource().getBindingContext("case").getPath());
+			oDetailContainer.bindElement("case>" + oEvent.getSource().getBindingContext("case").getPath().match(/\/\d+/)[0]);
 			oDetailContainer.setVisible(true);
 		},
 
@@ -67,9 +80,15 @@ sap.ui.define([
 			this.getView().byId("caseDetailContainer").setVisible(false);
 		},
 
-		onLevelChange:function(oEvent){
+		onChangeLevel:function(oEvent){
 			var sLevel = this.getView().byId("levelSelect").getSelectedKey();
-			this.getView().byId("areaRisk").getBinding("layers").filter(new sap.ui.model.Filter("Level", "EQ", sLevel));
+			this.getView().byId("blockRisk").getBinding("layers").filter(new sap.ui.model.Filter("Level", "EQ", sLevel));
+		},
+
+		onChangeCaseVisible:function(oEvent){
+			// if(!oEvent.getParameter("selected")){
+				this.getView().getModel("pageConfig").setProperty("/CaseTraceVisible",oEvent.getParameter("selected"));
+			// }
 		},
 
 		formatNumber: function (sValue) {
